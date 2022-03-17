5ee8cd4ee7fe01d3fd1da12c57dbce0b2fe8fcb9
Giant <83135006+Giant427@users.noreply.github.com>
2022-03-17 16:27:19 +0530

    Lil change

--- Mach/3d.cpp
+++ Mach/3d.cpp
@@ -1,29 +1,36 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <iostream>
 #include <string>
+#include <locale>
+#include <windows.h>
 using namespace std;
 
 const string separator = "========================================";
+const string whitespace = " ";
 
 const string volume_init = "Volume";
 const string tsa_init = "T.S.A";
 const string lsa_init = "L.S.A";
+const string csa_init = "C.S.A";
+const string fsa_init = "F.S.A";
 const string edges_init = "Total length of edges";
 
-const string pi = "pi";
-const string expo_square = " ^ 2 ";
-const string expo_cube = " ^ 3 ";
+const string pi_symbol = "π";
+const string expo_square = "²";
+const string expo_cube = "³";
 
 const string type_cuboid = "cuboid";
 const string type_cube = "cube";
 const string type_cylinder = "cylinder";
 const string type_hollow_cylinder = "hollow-cylinder";
 
-string generateWhitespace(int whitespace_index) {
-    string whitespace = " ";
-    for (int i = 1; i < whitespace_index; i++) {
-        whitespace = whitespace + " ";
+string generateDuplicates (string duplicate, int duplicate_index) {
+    string duplicated = duplicate;
+    for (int i = 1; i < duplicate_index; i++) {
+        duplicated = duplicated + duplicate;
     }
-    return whitespace;
+    return duplicated;
 }
 
 void cuboid () {
@@ -48,13 +55,13 @@ void cuboid () {
     volume = volume + " => l x b x h";
     cout << volume << "\n";
 
-    volume = generateWhitespace(volume_init.length()) + " => ";
+    volume = generateDuplicates(whitespace, volume_init.length()) + " => ";
     volume = volume + to_string(Length) + " x ";
     volume = volume + to_string(Breadth) + " x ";
     volume = volume + to_string(Height);
     cout << volume << "\n";
 
-    volume = generateWhitespace(volume_init.length()) + " => ";
+    volume = generateDuplicates(whitespace, volume_init.length()) + " => ";
     volume = volume + to_string(Length * Breadth * Height);
     cout << volume << "\n\n";
 
@@ -63,23 +70,23 @@ void cuboid () {
     tsa = tsa + " => 2(lb + bh + lh)";
     cout << tsa << "\n";
     
-    tsa = generateWhitespace(tsa_init.length()) + " => ";
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
     tsa = tsa + "2((" + to_string(Length) + " x " + to_string(Breadth)+ ") + ";
     tsa = tsa + "(" + to_string(Breadth) + " x " + to_string(Height)+ ") + ";
     tsa = tsa + "(" + to_string(Length) + " x " + to_string(Height)+ "))";
     cout << tsa << "\n";
 
-    tsa = generateWhitespace(tsa_init.length()) + " => ";
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
     tsa = tsa + "2(" + to_string(Length * Breadth) + " + ";
     tsa = tsa + to_string(Breadth * Height) + " + ";
     tsa = tsa + to_string(Length * Height) + ")";
     cout << tsa << "\n";
 
-    tsa = generateWhitespace(tsa_init.length()) + " => ";
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
     tsa = tsa + "2(" + to_string((Length * Breadth) + (Breadth * Height) + (Length * Height)) + ")";
     cout << tsa << "\n";
 
-    tsa = generateWhitespace(tsa_init.length()) + " => ";
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
     tsa = tsa + to_string(2 * ((Length * Breadth) + (Breadth * Height) + (Length * Height)));
     cout << tsa << "\n\n";
 
@@ -88,19 +95,19 @@ void cuboid () {
     lsa = lsa + " => 2(l + b)h";
     cout << lsa << "\n";
 
-    lsa = generateWhitespace(lsa_init.length()) + " => ";
+    lsa = generateDuplicates(whitespace, lsa_init.length()) + " => ";
     lsa = lsa + "2(" + to_string(Length) + " + " + to_string(Breadth) + ")" + to_string(Height);
     cout << lsa << "\n";
 
-    lsa = generateWhitespace(lsa_init.length()) + " => ";
+    lsa = generateDuplicates(whitespace, lsa_init.length()) + " => ";
     lsa = lsa + "2(" + to_string(Length + Breadth) + ")" + to_string(Height);
     cout << lsa << "\n";
 
-    lsa = generateWhitespace(lsa_init.length()) + " => ";
+    lsa = generateDuplicates(whitespace, lsa_init.length()) + " => ";
     lsa = lsa + to_string(2 * (Length + Breadth)) + " x " + to_string(Height);
     cout << lsa << "\n";
 
-    lsa = generateWhitespace(lsa_init.length()) + " => ";
+    lsa = generateDuplicates(whitespace, lsa_init.length()) + " => ";
     lsa = lsa + to_string((2 * (Length + Breadth)) * Height);
     cout << lsa << "\n\n";
 
@@ -109,17 +116,17 @@ void cuboid () {
     edges = edges + " => 4(l + b + h)";
     cout << edges << "\n";
 
-    edges = generateWhitespace(edges_init.length()) + " => ";
+    edges = generateDuplicates(whitespace, edges_init.length()) + " => ";
     edges = edges + "4(" + to_string(Length) + " + ";
     edges = edges + to_string(Breadth) + " + ";
     edges = edges + to_string(Height) + ")";
     cout << edges << "\n";
 
-    edges = generateWhitespace(edges_init.length()) + " => ";
+    edges = generateDuplicates(whitespace, edges_init.length()) + " => ";
     edges = edges + "4(" + to_string(Length + Breadth + Height) + ")";
     cout << edges << "\n";
 
-    edges = generateWhitespace(edges_init.length()) + " => ";
+    edges = generateDuplicates(whitespace, edges_init.length()) + " => ";
     edges = edges+ to_string(4 * (Length + Breadth + Height));
     cout << edges << "\n";
 
@@ -142,11 +149,11 @@ void cube () {
     volume = volume + " => s" + expo_cube;
     cout << volume << "\n";
 
-    volume = generateWhitespace(volume_init.length()) + " => ";
+    volume = generateDuplicates(whitespace, volume_init.length()) + " => ";
     volume = volume + to_string(Side) + expo_cube;
     cout << volume << "\n";
 
-    volume = generateWhitespace(volume_init.length()) + " => ";
+    volume = generateDuplicates(whitespace, volume_init.length()) + " => ";
     volume = volume + to_string(pow(Side, 3));
     cout << volume << "\n\n";
 
@@ -155,15 +162,15 @@ void cube () {
     tsa = tsa + " => 6s" + expo_square;
     cout << tsa << "\n";
 
-    tsa = generateWhitespace(tsa_init.length()) + " => ";
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
     tsa = tsa + "6 x " + to_string(Side) + expo_square;
     cout << tsa << "\n";
 
-    tsa = generateWhitespace(tsa_init.length()) + " => ";
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
     tsa = tsa + "6 x " + to_string(pow(Side, 2));
     cout << tsa << "\n";
 
-    tsa = generateWhitespace(tsa_init.length()) + " => ";
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
     tsa = tsa + to_string(6 * pow(Side, 2));
     cout << tsa << "\n\n";
 
@@ -172,15 +179,15 @@ void cube () {
     lsa = lsa + " => 4s" + expo_square;
     cout << lsa << "\n";
 
-    lsa = generateWhitespace(lsa_init.length()) + " => ";
+    lsa = generateDuplicates(whitespace, lsa_init.length()) + " => ";
     lsa = lsa + "4 x " + to_string(Side) + expo_square;
     cout << lsa << "\n";
 
-    lsa = generateWhitespace(lsa_init.length()) + " => ";
+    lsa = generateDuplicates(whitespace, lsa_init.length()) + " => ";
     lsa = lsa + "4 x " + to_string(pow(Side, 2));
     cout << lsa << "\n";
 
-    lsa = generateWhitespace(lsa_init.length()) + " => ";
+    lsa = generateDuplicates(whitespace, lsa_init.length()) + " => ";
     lsa = lsa + to_string(4 * pow(Side, 2));
     cout << lsa << "\n\n";
 
@@ -189,18 +196,155 @@ void cube () {
     edges = edges + " => 12s";
     cout << edges << "\n";
 
-    edges = generateWhitespace(edges_init.length()) + " => ";
+    edges = generateDuplicates(whitespace, edges_init.length()) + " => ";
     edges = edges + "12 x " + to_string(Side);
     cout << edges << "\n";
 
-    edges = generateWhitespace(edges_init.length()) + " => ";
+    edges = generateDuplicates(whitespace, edges_init.length()) + " => ";
     edges = edges + to_string(12 * Side);
     cout << edges << "\n";
 
     return;
 }
 
+void cylinder () {
+    double Height = 0;
+    double Radius = 0;
+    double pi_0 = 3.14;
+    double pi_1 = 22;
+    double pi_2 = 7;
+    string decimal_pi = "";
+    string volume = volume_init;
+    string tsa = tsa_init;
+    string csa = csa_init;
+    string fsa = fsa_init;
+
+    cout << "Pi(" + pi_symbol + "): 22\n       --\n       7\n";
+    cout << "Consider pi as 3.14 (y/n): ";
+    cin >> decimal_pi;
+    if (decimal_pi == "y") {
+        pi_1 = 3.14;
+        cout << "Pi(" << pi_symbol << "): " << to_string(pi_1) << "\n";
+    }
+
+    cout << "Height(h): ";
+    cin >> Height;
+    cout << "Radius(r): ";
+    cin >> Radius;
+    cout << "\n";
+
+    // Volume
+
+    volume = volume + " => " + pi_symbol + "r" + expo_square + "h";
+    cout << volume << "\n";
+
+    volume = generateDuplicates(whitespace, volume_init.length()) + " => ";
+    volume = volume + to_string(pi_1) + " x " + to_string(Radius) + expo_square + " x " + to_string(Height);
+    if (pi_1 == 22) {
+        size_t pi_denominator_pos = volume.find(to_string(pi_1));
+        volume = volume + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + generateDuplicates("-", to_string(pi_1).length()) + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + to_string(pi_2);
+    }
+    cout << volume << "\n";
+
+    volume = generateDuplicates(whitespace, volume_init.length()) + " => ";
+    volume = volume + to_string(pi_1) + " x " + to_string(pow(Radius, 2)) + " x " + to_string(Height);
+    if (pi_1 == 22) {
+        size_t pi_denominator_pos = volume.find(to_string(pi_1));
+        volume = volume + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + generateDuplicates("-", to_string(pi_1).length()) + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + to_string(pi_2);
+    }
+    cout << volume << "\n";
+
+    volume = generateDuplicates(whitespace, volume_init.length()) + " => ";
+    volume = volume + to_string(pi_0 * pow(Radius, 2) * Height) + "\n";
+    cout << volume << "\n";
+
+    // Total Surface Area
+
+    tsa = tsa + " => 2" + pi_symbol + "r(r + h)";
+    cout << tsa << "\n";
+
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
+    tsa = tsa + "2 x " + to_string(pi_1) + " x " + to_string(Radius) + "(" + to_string(Radius) + " + " + to_string(Height) + ")";
+    if (pi_1 == 22) {
+        size_t pi_denominator_pos = tsa.find(to_string(pi_1));
+        tsa = tsa + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + generateDuplicates("-", to_string(pi_1).length()) + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + to_string(pi_2);
+    }
+    cout << tsa << "\n";
+
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
+    tsa = tsa + "2 x " + to_string(pi_1) + " x " + to_string(Radius) + "(" + to_string(Radius + Height) + ")";
+    if (pi_1 == 22) {
+        size_t pi_denominator_pos = tsa.find(to_string(pi_1));
+        tsa = tsa + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + generateDuplicates("-", to_string(pi_1).length()) + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + to_string(pi_2);
+    }
+    cout << tsa << "\n";
+
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
+    tsa = tsa + "2 x " + to_string(pi_1) + " x " + to_string(Radius * (Radius + Height));
+    if (pi_1 == 22) {
+        size_t pi_denominator_pos = tsa.find(to_string(pi_1));
+        tsa = tsa + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + generateDuplicates("-", to_string(pi_1).length()) + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + to_string(pi_2);
+    }
+    cout << tsa << "\n";
+
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
+    tsa = tsa + to_string(pi_1) + " x " + to_string(2 * Radius * (Radius + Height));
+    if (pi_1 == 22) {
+        size_t pi_denominator_pos = tsa.find(to_string(pi_1));
+        tsa = tsa + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + generateDuplicates("-", to_string(pi_1).length()) + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + to_string(pi_2);
+    }
+    cout << tsa << "\n";
+
+    tsa = generateDuplicates(whitespace, tsa_init.length()) + " => ";
+    tsa = tsa + to_string(2 * pi_0 * Radius * (Radius + Height));
+    cout << tsa << "\n\n";
+
+    // Curved Surface Area
+
+    csa = csa + " => 2" + pi_symbol + "rh";
+    cout << csa << "\n";
+
+    csa = generateDuplicates(whitespace, csa_init.length()) + " => ";
+    csa = csa + "2 x " + to_string(pi_1) + " x " + to_string(Radius) + " x " + to_string(Height);
+    if (pi_1 == 22) {
+        size_t pi_denominator_pos = csa.find(to_string(pi_1));
+        csa = csa + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + generateDuplicates("-", to_string(pi_1).length()) + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + to_string(pi_2);
+    }
+    cout << csa << "\n";
+
+    csa = generateDuplicates(whitespace, csa_init.length()) + " => ";
+    csa = csa + "2 x " + to_string(pi_1) + " x " + to_string(Radius) + " x " + to_string(Height);
+    if (pi_1 == 22) {
+        size_t pi_denominator_pos = csa.find(to_string(pi_1));
+        csa = csa + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + generateDuplicates("-", to_string(pi_1).length()) + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + to_string(pi_2);
+    }
+    cout << csa << "\n";
+
+    csa = generateDuplicates(whitespace, csa_init.length()) + " => ";
+    csa = csa + "2 x " + to_string(pi_1) + " x " + to_string(Radius * Height);
+    if (pi_1 == 22) {
+        size_t pi_denominator_pos = csa.find(to_string(pi_1));
+        csa = csa + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + generateDuplicates("-", to_string(pi_1).length()) + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + to_string(pi_2);
+    }
+    cout << csa << "\n";
+
+    csa = generateDuplicates(whitespace, csa_init.length()) + " => ";
+    csa = csa + to_string(pi_1) + " x " + to_string(2 * Radius * Height);
+    if (pi_1 == 22) {
+        size_t pi_denominator_pos = csa.find(to_string(pi_1));
+        csa = csa + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + generateDuplicates("-", to_string(pi_1).length()) + "\n" + generateDuplicates(whitespace, pi_denominator_pos) + to_string(pi_2);
+    }
+    cout << csa << "\n";
+
+    csa = generateDuplicates(whitespace, csa_init.length()) + " => ";
+    csa = csa + to_string(2 * pi_0 * Radius * Height);
+    cout << csa << "\n";
+
+    return;
+}
+
 int main (int argc, char *argv[]) {
+    SetConsoleOutputCP(65001);
     string type_object = argv[1];
 
     cout << "\n" << separator << "\n\n";
@@ -213,6 +357,11 @@ int main (int argc, char *argv[]) {
     // cube
     if (type_object == type_cube) {
         cube();
+    }
+
+    // cylinder
+    if (type_object == type_cylinder) {
+        cylinder();
     }
 
     cout << "\n" << separator << "\n";
